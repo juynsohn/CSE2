49bcaf886e521bf0c454bdd86e3bf376515ecf6c
juynsohn <jus219@lehigh.edu>
2015-08-31 22:29:51 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Eddie Sohn
+//homework 1
+//WelcomeClass Java Program
+
+//define class
+public class WelcomeClass {
+    
+//main method
+public static void main(String[] args) {
+    
+//welcome message, lehigh ID and autobiographic statement
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-J--U--S--2--1--9-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This year I got accepted to Lehigh University.");
+    System.out.println("I feel proud of myself.");
+    
+    }
+}
