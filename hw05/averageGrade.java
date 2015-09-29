9b20fba093a5ef6977450512c17cf3d16166b841
juynsohn <jus219@lehigh.edu>
2015-09-29 14:14:22 +0000

    updated hw05

--- hw05/averageGrade.java
+++ hw05/averageGrade.java
@@ -6,6 +6,9 @@ Date : 2015 Sep 30
 Purpose : Ask grades to user and compute average grade when user input '999'
 */
 
+// *Because Lehigh coursesite shows grade to second decimal point, this program is also designed
+//  to round off the grades to two decimal places.
+
 //import scanner
 import java.util.Scanner;
 
@@ -22,28 +25,32 @@ public class averageGrade {
         //Declare variables
         //'totalGrade' for total amount of Grades
         //'a' to divide and get the average grade
-        int totalGrade = 0;
-        int a = 0;
+        //variables in 'double' form in order to get the decimal point
+        double totalGrade = 0;
+        double a = 0;
         
         while (true) {
             
             System.out.print("Input grade (Input '999' to stop) : ");
-            int grade = myScanner.nextInt();
+            double grade = myScanner.nextDouble();
             
             if (grade == 999 && totalGrade == 0) {
                 
                 System.out.println("Error : Input grades in order to get the average.");
+                break;
                 
             }
             
             if (grade == 999 && totalGrade != 0) {
                 
-                System.out.println("The average grade is : " + (totalGrade/a));
+                double averageGrade;
+                averageGrade = (totalGrade/a);
+                System.out.println("The average grade is : " + (Math.round(averageGrade*100d)/100d));
                 break;    
             
             }
             
-            if (100 < grade) {
+            if (grade < 0 || 100 < grade) {
                 
                 System.out.println("Error : Invalid grade");
                 
