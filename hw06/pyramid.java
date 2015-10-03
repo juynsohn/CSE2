9b116b1a80ad85970117726da377ba5a2be69175
juynsohn <jus219@lehigh.edu>
2015-10-03 06:58:52 +0000

    updated hw06

---
+++ hw06/pyramid.java
@@ -0,0 +1,44 @@
+/*
+Eddie (JunYoung) Sohn
+Homework 6
+File Name : pyramid.java
+Date : 2015 Oct 06
+Purpose : Ask size of pyramid to user and generate pyramid
+*/
+
+//import scanner
+import java.util.Scanner;
+
+//define class
+public class pyramid {
+    
+    //define method
+    public static void main(String[] args) {
+        
+        Scanner myScanner = new Scanner (System.in);
+        
+        System.out.println("What size (line) of pyramid do you want to generate ? ");
+        int pyramidSize = myScanner.nextInt();
+        
+        for (int a = 0; a < pyramidSize; a++) {
+            
+            for (int b = 1; b < pyramidSize - a; b++) {
+                
+                System.out.print(" ");
+                
+            }
+            
+            for (int c = 0; c < a*2 + 1; c++) {
+                
+                System.out.print("*");
+                
+            }
+            
+            System.out.println();
+            
+        }
+        
+    }
+        
+}
+    
